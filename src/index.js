30520d3ea36064e0608c15266c97598098b4fc06
Luke Edwards <luke.edwards05@gmail.com>
2018-12-24 13:18:05 -0800

    import module logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+function toVal(mix) {
+	var k, y, str='';
+	if (mix) {
+		if (typeof mix === 'object') {
+			for (k in mix) {
+				if (mix[k] && (y = toVal(!!mix.push ? mix[k] : k))) {
+					str && (str += ' ');
+					str += y;
+				}
+			}
+		} else if (typeof mix !== 'boolean') {
+			str && (str += ' ');
+			str += mix;
+		}
+	}
+	return str;
+}
+
 export default function () {
-	//
+	var i=0, x, str='';
+	while (i < arguments.length) {
+		if (x = toVal(arguments[i++])) {
+			str && (str += ' ');
+			str += x
+		}
+	}
+	return str;
 }
